88d8574474d59abc876226c9f1b61e2f9152af19
mr6439 <98670531+mr6439@users.noreply.github.com>
2022-02-21 16:20:56 -0500

    Port

--- solution.py
+++ solution.py
@@ -6,7 +6,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
    endmsg = "\r\n.\r\n"
 
    # Choose a mail server (e.g. Google mail server) if you want to verify the script beyond GradeScope
-   mailserver = ("mail.smtp2go.com", 2525)
+   mailserver = ("smtp.gmail.com", 2525)
    # Create socket called clientSocket and establish a TCP connection with mailserver and port
 
    # Fill in start
@@ -15,16 +15,16 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
    clientSocket.connect(mailserver)
    
    recv = clientSocket.recv(1024).decode()
-   print(recv)
-   if recv[:3] != '220':
+   #print(recv)
+   #if recv[:3] != '220':
        #print('220 reply not received from server.')
 
    # Send HELO command and print server response.
    heloCommand = 'HELO Alice\r\n'
    clientSocket.send(heloCommand.encode())
    recv1 = clientSocket.recv(1024).decode()
-   print(recv1)
-   if recv1[:3] != '250':
+   #print(recv1)
+   #if recv1[:3] != '250':
        #print('250 reply not received from server.')
 
    # Send MAIL FROM command and print server response.
